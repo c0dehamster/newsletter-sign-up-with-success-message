fa707b436f91534918d7370ae96790ecb3fa6b3d
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-09-20 15:23:28 +0500

    Add conditional rendering of cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { SignUp } from "./components/SignUp"
 import { Success } from "./components/Success"
 
 function App() {
+	const signed = true
+
 	return (
 		<div className="app">
-			<SignUp></SignUp>
-			<Success></Success>
+			{!signed ? <SignUp></SignUp> : <Success></Success>}
 		</div>
 	)
 }
