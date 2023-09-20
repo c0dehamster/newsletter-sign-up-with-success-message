1dc7b02e1616b0c8aaec116c2a77bec9dcee4810
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-09-20 13:29:33 +0500

    Layout for the success message (mobile)

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -3,24 +3,26 @@ import "./Success.css"
 export const Success = () => {
 	return (
 		<div className="success">
-			<img
-				src="./images/icon-success.svg"
-				alt=""
-				className="success__icon"
-			/>
+			<div className="success__contents">
+				<img
+					src="./images/icon-success.svg"
+					alt=""
+					className="success__icon"
+				/>
 
-			<h2 className="success__heading">Thanks for subscribing!</h2>
+				<h2 className="success__heading">Thanks for subscribing!</h2>
 
-			<p className="success__details">
-				A confirmation email has been sent to{" "}
-				<a
-					href="mailto:ash@loremcompany.com"
-					className="success__email-link">
-					ash@loremcompany.com
-				</a>
-				. Please open it and click the button inside to confirm your
-				subscription.
-			</p>
+				<p className="success__details">
+					A confirmation email has been sent to{" "}
+					<a
+						href="mailto:ash@loremcompany.com"
+						className="success__email-link">
+						ash@loremcompany.com
+					</a>
+					. Please open it and click the button inside to confirm your
+					subscription.
+				</p>
+			</div>
 
 			<button className="button">Dismiss message</button>
 		</div>
